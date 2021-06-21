6dd0207e6590800bc3e170dba55e8fceb80e84c9
Daniel Ixcaquic <danielalexanderib73@gmail.com>
2021-06-21 16:56:25 -0600

    Otro cambio

--- src/principal/Juego.java
+++ src/principal/Juego.java
@@ -70,15 +70,6 @@ public class Juego {
         turno(true);
         turno(false);
 
-        /*
-         * t.pintarTablero(); t.moverFicha("09", true, new Coordenada(0, 3));
-         * t.pintarTablero(); t.moverFicha("09", true, new Coordenada(1, 4));
-         * t.pintarTablero(); t.getFicha("02", false).getMovimientosPosibles();
-         * //t.getCelda(new Coordenada(1, 2)).getFicha().getMovimientosPosibles();
-         * //System.out.println(t.moverFicha(0, 0, 5, 5)); //t.pintarTablero();
-         * //System.out.println(t.moverFicha(5, 5, 1, 1)); //t.pintarTablero();
-         */
-
     }
 
     private void turno(boolean esBlanca) {
